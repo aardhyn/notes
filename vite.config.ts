717a67644fb37bf8ad72d5102c3ed08248c84b8f
Aardhynlavneder <aardhyn@gmail.com>
2023-12-16 17:11:43 +1300

    build: env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dotenv from "dotenv";
+
+const path = ".env";
+dotenv.config({ path });
+
+const DEFAULT_PORT = 2048;
+const port = parseInt(process.env.VITE_PORT) || DEFAULT_PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: { port },
 });
