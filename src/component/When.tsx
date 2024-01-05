35cf82be4cc9a15ce15efa0eca739f1437760c24
Aardhynlavneder <aardhyn@gmail.com>
2024-01-06 10:47:55 +1300

    chore: tighten implementation

--- src/component/When.tsx
+++ src/component/When.tsx
@@ -1,5 +1,12 @@
-import { ReactNode } from "react";
+import { HTMLAttributes, ReactNode, forwardRef } from "react";
 import { s, CSS } from "style/stitches.config";
+
+type WhenProps = {
+  selector: `${string} &` | boolean;
+  children?: ReactNode;
+  css?: CSS;
+  fallback?: ReactNode;
+} & HTMLAttributes<HTMLDivElement>;
 
 /**
  * Render children when selector matches
@@ -7,45 +14,43 @@ import { s, CSS } from "style/stitches.config";
  * @param children children to render
  * @param fallback fallback to render when selector does not match
  */
-export function When({
-  selector,
-  children,
-  css,
-  fallback,
-}: {
-  selector: string | boolean;
-  children: ReactNode;
-  css?: CSS;
-  fallback?: ReactNode;
-}) {
-  if (typeof selector === "boolean") {
-    return <s.div css={css}>{selector ? children : fallback}</s.div>;
-  }
+export const When = forwardRef<HTMLDivElement, WhenProps>(
+  ({ selector, children, css, fallback }, ref) => {
+    if (typeof selector === "boolean") {
+      return (
+        <s.div ref={ref} css={css}>
+          {selector ? children : fallback}
+        </s.div>
+      );
+    }
 
-  return (
-    <>
-      <s.div
-        css={{
-          d: "none",
-          [selector]: {
-            d: "block",
-            ...css,
-          },
-        }}
-      >
-        {children}
-      </s.div>
-      {fallback && (
+    return (
+      <>
         <s.div
+          ref={ref}
           css={{
-            d: "block",
-            ...css,
-            [selector]: { d: "none" },
+            d: "none",
+            [selector]: {
+              d: "block",
+              ...css,
+            },
           }}
         >
-          {fallback}
+          {children}
         </s.div>
-      )}
-    </>
-  );
-}
+        {fallback && (
+          <s.div
+            ref={ref}
+            css={{
+              d: "block",
+              ...css,
+              [selector]: { d: "none" },
+            }}
+          >
+            {fallback}
+          </s.div>
+        )}
+      </>
+    );
+  }
+);
