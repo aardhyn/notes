7f10e401b237beb79d34c5b5fbaed0bf80a4fdc9
Aardhynlavneder <aardhyn@gmail.com>
2024-01-20 11:38:47 +1300

    chore: improved shortcut registration

--- src/api/shortcut.tsx
+++ src/api/shortcut.tsx
@@ -1,34 +1,83 @@
 import { Character } from "constant/types";
+import { invariant } from "exception/invariant";
 import { useEffect } from "react";
-import { capitalize, caselessEquality } from "util/string";
+import { caselessEquality } from "util/string";
 
-const modifierKeys = ["alt", "ctrl", "meta", "shift"] as const;
+const modifierKeys = [
+  "Alt",
+  "Control",
+  "Meta",
+  "Shift",
+  "OS",
+  "Hyper",
+  "Super",
+  "ScrollLock",
+  "CapsLock",
+  "NumLock",
+  "AltGraph",
+  "Fn",
+  "FnLock",
+  "Symbol",
+  "SymbolLock",
+] as const;
 type Modifier = (typeof modifierKeys)[number];
 
+export type ArrowKey = "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight";
+type ShortcutKey = Character | ArrowKey | "enter" | "escape" | "space";
+
+type ShortcutOptions = Partial<{ enabled: boolean; modifiers?: Modifier[] }>;
+
+const SHORTCUT_EVENT = "keydown";
+
+function createShortcut(
+  fn: () => void,
+  key: ShortcutKey,
+  modifiers: ShortcutOptions["modifiers"]
+) {
+  const handleKeyDown = (event: Event) => {
+    invariant(event instanceof KeyboardEvent, "not keyboard event");
+
+    const isModified =
+      modifiers?.every((modifier) => event.getModifierState(modifier)) ?? true;
+    const isKey = caselessEquality(event.key, key);
+
+    if (isModified && isKey) {
+      event.preventDefault();
+      event.stopPropagation();
+      fn();
+    }
+  };
+
+  return {
+    register() {
+      document.addEventListener(SHORTCUT_EVENT, handleKeyDown, false);
+    },
+    deregister() {
+      document.removeEventListener(SHORTCUT_EVENT, handleKeyDown, false);
+    },
+  };
+}
+
+/**
+ * Bind a function to a keyboard shortcut in either
+ * @param fn function to call when shortcut is pressed
+ * @param key key to listen for
+ * @param modifiers modifiers to listen for
+ */
 export function useShortcut(
   fn: () => void,
-  key: Character,
-  modifiers?: Modifier[]
+  key: ShortcutKey,
+  { modifiers, enabled }: ShortcutOptions = {}
 ) {
   useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      const isModified =
-        modifiers?.every((modifier) =>
-          event.getModifierState(capitalize(modifier))
-        ) ?? true;
-      const isKey = caselessEquality(event.key, key);
+    const { register, deregister } = createShortcut(fn, key, modifiers);
+    if (enabled ?? true) {
+      deregister();
+      register();
+    } else {
+      deregister();
+    }
 
-      if (isModified && isKey) {
-        console.log("shortcut", key, modifiers);
-        event.preventDefault();
-        event.stopPropagation();
-        fn();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [fn, key, modifiers]);
+    return deregister;
+  }, [fn, key, enabled, modifiers]);
 }
