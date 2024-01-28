f4025b7e1cee564b38b7dec45d5cf2c570856a3b
Aardhynlavneder <aardhyn@gmail.com>
2024-01-28 18:05:21 +1300

    feat: remove persistent user data storage

--- src/route/AuthRedirect.tsx
+++ src/route/AuthRedirect.tsx
@@ -1,9 +1,16 @@
-import { useAuth } from "api";
+import { useUser } from "api";
+import { LoadingScrim } from "component";
 import { Navigate, Outlet } from "react-router-dom";
 
 export function AuthRedirect() {
-  const { user } = useAuth();
+  const { data: user, isLoading } = useUser();
 
+  // user for user?
+  if (isLoading) return <LoadingScrim />;
+
+  // no user? redirect to sign in
   if (!user) return <Navigate to="/sign-in" />;
-  else return <Outlet />;
+
+  // user is logged in, render authenticated routes
+  return <Outlet />;
 }
